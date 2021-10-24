f30095bf5ce891bcf5f15a2ef4039595fa071178
Raiane Caroline <raianecaroline.st@gmail.com>
2021-10-24 11:24:11 -0300

    Crachá NLW-WEB

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'Raiannecaroline',
+  linkedin: 'in/raiannecaroline',
+  facebook: 'raianecarolinest',
+  instagram: 'raiannecaroline_',
+  twitter: 'Raiannecaroline'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
